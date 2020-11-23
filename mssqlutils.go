8a4f73eea4d4fcef23c4efdb4ca934184147d266
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-11-23 15:10:46 +0200

    first commit

--- mssqlutils.go
+++ mssqlutils.go
@@ -9,12 +9,17 @@ import (
 )
 
 // ConnectOrDie func
-func ConnectOrDie(server string, port int8, user string, password string, database string, encrypt bool, trust bool) *sql.DB {
+func ConnectOrDie(server string, port string, user string, password string, database string, encrypt bool, trust bool) *sql.DB {
+
+	portNumber, err := strconv.Atoi(port)
+	if err != nil {
+		log.Fatalf("Failed creating SQL connection pool, illegal port: %s", port)
+	}
 
 	var db *sql.DB
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;encrypt=%s;TrustServerCertificate=%s", server, user, password, port, database, strconv.FormatBool(encrypt), strconv.FormatBool(trust))
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;encrypt=%s;TrustServerCertificate=%s", server, user, password, portNumber, database, strconv.FormatBool(encrypt), strconv.FormatBool(trust))
 
-	db, err := sql.Open("sqlserver", connString)
+	db, err = sql.Open("sqlserver", connString)
 	if err != nil {
 		log.Fatalf("Failed creating SQL connection pool: %s", err)
 	}
